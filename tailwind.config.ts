2ec68db0c36c56189bb337d6616ccec2d9c917c1
Daiane Bolzan <dainha85@gmail.com>
2024-01-26 12:38:42 -0300

    feat: add base color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,23 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "background": "url('/fundo2.png')",
-        "background-2":
-          "url('/bubble1.png')",
-        "background-3":
-          "url('/limpeza1.png')",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				'base': '#C5E7EB',
+			},
+			backgroundImage: {
+				background: "url('/fundo2.png')",
+				'background-2': "url('/bubble1.png')",
+				'background-3': "url('/limpeza1.png')",
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
