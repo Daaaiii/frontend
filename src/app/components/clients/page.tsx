8b754c2da5467596acef2b504811e3c62dfa87b9
Daiane Bolzan <dainha85@gmail.com>
2024-01-26 19:34:55 -0300

    feat: add sort and filter

--- src/app/components/clients/page.tsx
+++ src/app/components/clients/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { SetStateAction, useEffect, useState } from 'react';
 import Image from 'next/image';
 import ClientUpdateForm from '../Cadastro/clientUpdateForm';
 
@@ -18,14 +18,17 @@ export default function Client() {
 	const [isErrorModalOpen, setIsErrorModalOpen] = useState(false);
 	const [errorMessage, setErrorMessage] = useState('');
 	const [isEditModalOpen, setIsEditModalOpen] = useState(false);
-	const [editingClientId, setEditingClientId] = useState<Client| null>(null);
+	const [editingClientId, setEditingClientId] = useState<Client | null>(null);
+	const [sortField, setSortField] = useState<keyof Client | null>(null);
+	const [sortDirection, setSortDirection] = useState<'asc' | 'desc'>('asc');
+	const [filter, setFilter] = useState('');
+    const [filteredClients, setFilteredClients] = useState<Client[]>([]);
 
 	const editClient = async (client: Client) => {
 		setEditingClientId(client);
 		setIsEditModalOpen(true);
 	};
-	const deleteClient = async(clientId:number)=>{
-		
+	const deleteClient = async (clientId: number) => {
 		try {
 			const apiUrl = `http://localhost:3001/client/${clientId}`;
 			const authToken = localStorage.getItem('token');
@@ -33,10 +36,10 @@ export default function Client() {
 				headers: {
 					Authorization: `Bearer ${authToken}`,
 				},
-			};			
-			
+			};
+
 			const response = await axios.delete(apiUrl, headers);
-			
+
 			if (response.status === 200) {
 				fetchClients();
 			}
@@ -46,7 +49,7 @@ export default function Client() {
 				'Erro ao processar a solicitação. Tente novamente mais tarde.'
 			);
 		}
-	}
+	};
 
 	const fetchClients = async () => {
 		try {
@@ -77,18 +80,106 @@ export default function Client() {
 		fetchClients();
 	}, []);
 
+	const onSortChange = (field: keyof Client) => {
+		if (sortField === field) {
+			setSortDirection(sortDirection === 'asc' ? 'desc' : 'asc');
+		} else {
+			setSortField(field);
+			setSortDirection('asc');
+		}
+
+		setClients(sortClients(clients, field, sortDirection));
+	};
+
+	const sortClients = (
+		clients: Client[],
+		field: keyof Client,
+		direction: 'asc' | 'desc'
+	) => {
+		return [...clients].sort((a, b) => {
+			if (a[field] < b[field]) {
+				return direction === 'asc' ? -1 : 1;
+			}
+			if (a[field] > b[field]) {
+				return direction === 'asc' ? 1 : -1;
+			}
+			return 0;
+		});
+	};
+	const applyFilter = () => {
+       
+        const filtered = clients.filter(client => 
+            client.name.toLowerCase().includes(filter.toLowerCase()) ||
+            client.id.toString().includes(filter)
+        );
+        setFilteredClients(filtered);
+    }
+
+    const handleFilterChange = (event: { target: { value: SetStateAction<string>; }; }) => {
+        setFilter(event.target.value);
+    };
+
 	return (
 		<div className='relative  flex flex-col h-screen w-full items-start justify-start p-4 m-4 rounded-md bg-base'>
 			<h1 className='w-full flex items-start justify-center text-blue-500 text-2xl font-bold p-4 mb-2'>
 				Clientes
 			</h1>
+			<div className='mb-4 flex'>
+                <input
+                    type="text"
+                    placeholder="Filtrar por nome ou ID..."
+                    value={filter}
+                    onChange={handleFilterChange}
+                    className="p-2 border-2 border-gray-300 rounded-md"
+                />
+                <button
+                    onClick={applyFilter}
+                    className='ml-2 bg-blue-500 text-white p-2 rounded-md'
+                >
+                    Filtrar
+                </button>
+            </div>
+
 			<div className='w-full overflow-x-auto'>
-				<table className='table-auto w-full '>
+				<table className='table-auto w-full'>
 					<thead>
 						<tr className='justify-evenly text-gray-500 pb-4 pt-4 text-center align-middle'>
-							<th className='pb-2'>ID</th>
-							<th className='pb-2'>Nome</th>
-							<th className='pb-2'>Telefone</th>
+							<th
+								className='pb-2'
+								onClick={() => onSortChange('id')}>
+								<p>ID</p>
+								<Image
+									alt='icon'
+									src={
+										sortField === 'name' &&
+										sortDirection === 'asc'
+											? '/sort-up-solid.svg'
+											: '/sort-down-solid.svg'
+									}
+									width={10}
+									height={10}
+								/>
+							</th>
+							<th
+								className='pb-2'
+								onClick={() => onSortChange('name')}>
+								<p>Nome</p>
+								<Image
+									alt='icon'
+									src={
+										sortField === 'name' &&
+										sortDirection === 'asc'
+											? '/sort-up-solid.svg'
+											: '/sort-down-solid.svg'
+									}
+									width={10}
+									height={10}
+								/>
+							</th>
+							<th
+								className='pb-2'>
+								Telefone								
+							</th>
 							<th className='pb-2'>Coord X</th>
 							<th className='pb-2'>Coord Y</th>
 							<th className='pb-2'>Editar</th>
@@ -96,7 +187,7 @@ export default function Client() {
 						</tr>
 					</thead>
 					<tbody>
-						{clients.map((client) => (
+						{filteredClients.map((client) => (
 							<tr
 								key={client.id}
 								className='items-center text-gray-500 pb-4 pt-4 text-center align-middle'>
@@ -116,7 +207,8 @@ export default function Client() {
 									</button>
 								</td>
 								<td className='pb-2'>
-									<button onClick={()=>deleteClient(client.id)}>
+									<button
+										onClick={() => deleteClient(client.id)}>
 										<Image
 											alt='icon'
 											src='/trash-can-solid.svg'
